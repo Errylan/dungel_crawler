1b68e3e724ff6c702e4ed576f51aef08533c834d
Jhenyfer Ketllyn de Oliveira Martins <20261198060031@ifrn.local>
2026-09-14 07:43:50 -0300

    Atualiza classe Dificuldade

--- src/dungeon/Dificuldade.java
+++ src/dungeon/Dificuldade.java
@@ -1,1 +1,23 @@
+public enum Dificuldade {
 
+    FACIL(1),
+
+    MEDIO(2),
+
+    DIFICIL(3);
+
+    private final int nivel;
+
+    Dificuldade(int nivel) {
+
+        this.nivel = nivel;
+
+    }
+
+    public int getNivel() {
+
+        return nivel;
+
+    }
+
+}
